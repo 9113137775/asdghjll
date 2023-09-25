bae65fee4211dabc023390a7172e4074ac3ad7d9
Pranav <105210844+9113137775@users.noreply.github.com>
2023-09-25 21:12:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function dark() {
+    document.getElementById("dark").style = "background-color: darkorange; width:100%; color: white;";
+}
+function leave() {
+    document.getElementById("dark").style = "background-color: orange; width:100%; color: white;";
+
+}
